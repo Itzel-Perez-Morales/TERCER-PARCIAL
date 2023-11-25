3b9fb34d311ac2543d806cbff0123c2cd2d1482a
Itzel-Perez-Morales <itzel.perezmrl@uanl.edu.mx>
2023-11-25 17:17:41 -0600

    init

---
+++ 3 PARCIAL/EXAMEN 3P.cpp
@@ -0,0 +1,348 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <CommDlg.h>
+#include "resource.h"
+#include "Commctrl.h"
+
+using namespace std;
+
+struct Usuario
+{
+	int id_usuario;
+	string clave;
+	string contrasena;
+
+	Usuario(int id, string clave, string contrasena) { //constructor
+		id_usuario = id;
+		this->clave = clave;
+		this->contrasena = contrasena;
+	}
+};
+
+struct Evento
+{
+	int id_evento;
+	string nombre;
+	string descripcion;
+	string fecha;
+	string hora;
+	string lugar;
+	float precio;
+	int capacidad; //cuantos tickets estan disponibles
+
+	Evento()
+	{
+		id_evento = 0;
+		nombre = "";
+		descripcion = "";
+		fecha = "";
+		hora = "";
+		lugar = "";
+		precio = 0;
+		capacidad = 0;
+	};
+
+	Evento(int id_evento, string nombre, string descripcion, string fecha, string hora, string lugar, float precio, int capacidad) // se llenan de los datos mandados
+	{
+		this->id_evento = id_evento;
+		this->nombre = nombre;
+		this->descripcion = descripcion;
+		this->fecha = fecha;
+		this->lugar = lugar;
+		this->hora = hora;
+		this->precio = precio;
+		this->capacidad = capacidad;
+	}
+};
+
+struct Boletos
+{
+	int id_boleto;
+	Evento evento; // se referencia a que boleto va el evento -> se enlaza
+	float monto;
+
+	Boletos() {
+
+	}
+
+	Boletos(int id, Evento evento, float precioFinal) {
+		this->id_boleto = id;
+		this->evento = evento;
+		this->monto = precioFinal;
+	}
+};
+
+Usuario usuarioRegistrado = Usuario(1, "1000001", "1234");
+
+Evento evento1 = Evento(1, "1", "d1", "26/11/2023", "09:00:00", "UANL", 350.50, 100); // ORDENAR
+Evento evento2 = Evento(2, "2", "d2", "26/11/2023", "10:00:00", "Alameda", 100.00, 200);
+Evento evento3 = Evento(3, "3", "d3", "28/11/2023", "13:00:00", "Sendero", 20.0, 500);
+Evento evento4 = Evento(4, "4", "d4", "27/11/2023", "15:00:00", "UANL", 250.50, 90);
+Evento evento5 = Evento(5, "5", "d5", "27/11/2023", "17:00:00", "Plaza Sesamo", 630.50, 30);
+Evento evento6 = Evento(6, "6", "d6", "27/11/2023", "19:00:00", "Macroplaza", 550.50, 500);
+
+Evento eventos[6] = { evento1, evento2, evento3, evento4, evento5, evento6 };
+Evento eventoSeleccionado;
+
+Boletos compras[20];
+Boletos boletoSeleccionado;
+int contadorCompras = 0;
+
+HWND hLogInWnd;
+HWND hCompraWnd;
+HWND hMisComprasWnd;
+HWND hAgendaWnd;
+
+void MenuOpcion(HWND hwnd, WPARAM wparam, HWND ventanaCerrar);
+
+LRESULT CALLBACK PROCLogIn(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam); //declaracion de la primer ventana
+LRESULT CALLBACK PROCEventos(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK PROCCompra(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK PROCMisCompras(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+
+LRESULT CALLBACK PROCLogIn(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_SHOWWINDOW:
+	{
+		SetDlgItemText(hwnd, IDC_EDIT_LogIn_ClaveUsu, "1000001");
+		SetDlgItemText(hwnd, IDC_EDIT_LogIn_Contrasena, "1234");
+	}	break;
+
+	case WM_COMMAND:
+	{
+		if (LOWORD(wParam) == IDOK)
+		{
+			//Se obtiene la clave y contraseña ingresada en el edit
+			char* usuario = new char[20];
+			char* contra = new char[20];
+			GetWindowText(GetDlgItem(hwnd, IDC_EDIT_LogIn_ClaveUsu), usuario, 20);
+			GetWindowText(GetDlgItem(hwnd, IDC_EDIT_LogIn_Contrasena), contra, 20);
+							
+			// Logica para comprobar credenciales
+			if (usuarioRegistrado.clave == usuario && usuarioRegistrado.contrasena == contra)
+			{
+				ShowWindow(hLogInWnd, 0);
+				ShowWindow(hAgendaWnd, 1);
+			}
+			else
+			{
+				MessageBox(hwnd, "Datos inválidos", "A D V E R T E N C I A", MB_ICONEXCLAMATION  | MB_OK);
+			}
+		}
+	}break;
+	}
+	return 0;
+}
+
+
+
+LRESULT CALLBACK PROCEventos(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_SHOWWINDOW:
+		SendMessage(GetDlgItem(hwnd, IDC_LISTBOX_Eventos), LB_RESETCONTENT, 0, 0); //elimina los valores del cuadro de text
+		for (int i = 0; i < 6; i++)
+		{
+			string cadena;
+			cadena.append(eventos[i].lugar);
+			cadena.append(" | ");
+			cadena.append(eventos[i].fecha);
+			cadena.append(" | ");
+			cadena.append(eventos[i].hora);
+			
+			const char* valor = cadena.c_str();
+			SendMessage(GetDlgItem(hwnd, IDC_LISTBOX_Eventos), LB_ADDSTRING, 0, (LPARAM)valor);
+		}
+		break;
+
+	case WM_COMMAND:
+		MenuOpcion(hwnd, wParam, hAgendaWnd);
+		if (LOWORD(wParam) == IDC_LISTBOX_Eventos && HIWORD(wParam) == LBN_SELCHANGE)
+		{
+			int indiceEvento = SendDlgItemMessage(hwnd, IDC_LISTBOX_Eventos, LB_GETCURSEL, 0, 0);
+			eventoSeleccionado = eventos[indiceEvento];
+
+			// Mostrar datos
+			SetDlgItemText(hwnd, IDC_STATIC_Eventos_Fecha, (LPCSTR)eventos[indiceEvento].fecha.c_str());
+			SetDlgItemText(hwnd, IDC_STATIC_Eventos_Hora, (LPCSTR)eventos[indiceEvento].hora.c_str());
+			SetDlgItemText(hwnd, IDC_STATIC_Eventos_Obra, (LPCSTR)eventos[indiceEvento].nombre.c_str());
+			SetDlgItemText(hwnd, IDC_STATIC_Eventos_Locacion, (LPCSTR)eventos[indiceEvento].lugar.c_str());
+		}
+
+		if (LOWORD(wParam) == IDC_BUTTON_Eventos_Comprar) 
+		{
+			int indice = SendDlgItemMessage(hwnd, IDC_LISTBOX_Eventos, LB_GETCURSEL, 0, 0);
+			if (indice == -1)
+			{
+				MessageBox(hwnd, "No se ha seleccionado un evento", "(!) A D V E R T E N C I A", MB_ICONERROR | MB_OK);
+				break;
+			}
+			eventoSeleccionado = eventos[indice];
+			ShowWindow(hAgendaWnd, 0);
+			ShowWindow(hCompraWnd, 1);
+		}
+		break;
+	}
+	return 0;
+}
+
+
+
+LRESULT CALLBACK PROCCompra(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) 
+{
+	switch(msg) 
+	{
+	case WM_SHOWWINDOW:
+	{
+		SendMessage(GetDlgItem(hwnd, IDC_COMBO1), CB_RESETCONTENT, 0, 0);
+		SendMessage(GetDlgItem(hwnd, IDC_COMBO1), CB_ADDSTRING, 0, LPARAM(" | Sin promoción"));
+		SendMessage(GetDlgItem(hwnd, IDC_COMBO1), CB_ADDSTRING, 0, LPARAM(" | Promoción 2 x 1"));
+		SendMessage(GetDlgItem(hwnd, IDC_COMBO1), CB_ADDSTRING, 0, LPARAM(" | Promoción 10%" ));
+		SendMessage(GetDlgItem(hwnd, IDC_COMBO1), CB_SETCURSEL, 0, 0);
+		
+		SetDlgItemText(hwnd, IDC_STATIC_Compra_NombreObra, (LPCSTR)eventoSeleccionado.nombre.c_str());
+		SetDlgItemText(hwnd, IDC_STATIC_Compra_Descripcion, (LPCSTR)eventoSeleccionado.descripcion.c_str());
+		SetDlgItemText(hwnd, IDC_STATIC_Compras_Lugar, (LPCSTR)eventoSeleccionado.lugar.c_str());
+		SetDlgItemText(hwnd, IDC_STATIC_Compra_FECHA, (LPCSTR)eventoSeleccionado.fecha.c_str());
+		SetDlgItemText(hwnd, IDC_STATIC_Compra_Hora, (LPCSTR)eventoSeleccionado.hora.c_str());
+
+		// Calcular precio final
+		float precioFinal = eventoSeleccionado.precio * 1.16;
+		SetDlgItemText(hwnd, IDC_STATIC_Compra_PrecioFinal, (LPCSTR)to_string(precioFinal).c_str());
+		
+	} break;
+
+	case WM_COMMAND:
+		MenuOpcion(hwnd, wParam, hCompraWnd);
+		if (LOWORD(wParam) == IDC_COMBO1 && HIWORD(wParam) == CBN_CLOSEUP)
+		{
+			// Recalcular precio final
+			// Aplicar promo / descuento
+			int indicePromo = SendDlgItemMessage(hwnd, IDC_COMBO1, CB_GETCURSEL, 0, 0);
+			float precioFinal = eventoSeleccionado.precio;
+			switch (indicePromo) {
+			case 0: case 1:
+				precioFinal = precioFinal * 1.16;
+				break;
+			case 2:
+				precioFinal = precioFinal * 1.16 * 0.9;
+				break;
+			}
+			SetDlgItemText(hwnd, IDC_STATIC_Compra_PrecioFinal, (LPCSTR)to_string(precioFinal).c_str());
+		}
+
+		if (LOWORD(wParam) == IDOK_Confirmar)
+		{
+			char* buffer = new char[256];
+			GetDlgItemText(hwnd, IDC_STATIC_Compra_PrecioFinal, buffer, 256);
+			float precioFinal = atof(buffer);
+			compras[contadorCompras] = Boletos(contadorCompras, eventoSeleccionado, precioFinal);
+			
+			contadorCompras++;
+			ShowWindow(hCompraWnd, 0);
+			ShowWindow(hAgendaWnd, 1);
+			MessageBox(hwnd, "La compra se ha realizado correctamente", "T A Q U I L L A", MB_ICONEXCLAMATION | MB_OK);
+		}
+
+		if (LOWORD(wParam) == IDCANCEL_Compra)
+		{
+			ShowWindow(hCompraWnd, 0);
+			ShowWindow(hAgendaWnd, 1);
+			MessageBox(hwnd, "La compra se ha cancelado", "A D V E R T E N C I A", MB_ICONWARNING | MB_OK);
+		}
+		break;
+	}
+	return 0;
+}
+
+
+
+LRESULT CALLBACK PROCMisCompras(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_SHOWWINDOW:
+		SendMessage(GetDlgItem(hwnd, IDC_LISTBOX_Boletos), LB_RESETCONTENT, 0, 0); //elimina los valores del cuadro de text
+		for (int i = 0; i < contadorCompras; i++)
+		{
+			string cadena;
+			cadena.append(compras[i].evento.lugar);
+			cadena.append(" | ");
+			cadena.append(compras[i].evento.fecha);
+			cadena.append(" | ");
+			cadena.append(compras[i].evento.hora);
+			cadena.append(" | ");
+			cadena.append(to_string(compras[i].monto).c_str());
+
+
+			const char* valor = cadena.c_str();
+			SendMessage(GetDlgItem(hwnd, IDC_LISTBOX_Boletos), LB_ADDSTRING, 0, (LPARAM)valor);
+		}
+		break;
+
+		break;
+	case WM_COMMAND:
+	{
+		MenuOpcion(hwnd, wParam, hMisComprasWnd);
+		if (LOWORD(wParam) == IDC_LISTBOX_Boletos && HIWORD(wParam) == LBN_SELCHANGE)
+		{
+			int indiceBoleto = SendDlgItemMessage(hwnd, IDC_LISTBOX_Boletos, LB_GETCURSEL, 0, 0);
+			boletoSeleccionado = compras[indiceBoleto];
+
+			// Mostrar datos
+			SetDlgItemText(hwnd, IDC_STATIC_MisCompras_Fecha, (LPCSTR)boletoSeleccionado.evento.fecha.c_str());
+			SetDlgItemText(hwnd, IDC_STATIC_MisCompras_Hora, (LPCSTR)boletoSeleccionado.evento.hora.c_str());
+			SetDlgItemText(hwnd, IDC_STATIC_MisCompras_Nombre, (LPCSTR)boletoSeleccionado.evento.nombre.c_str());
+			SetDlgItemText(hwnd, IDC_STATIC_MisCompras_Lugar, (LPCSTR)boletoSeleccionado.evento.lugar.c_str());
+		}
+
+	}
+	break;
+	}
+	return 0;
+}
+
+void MenuOpcion(HWND hwnd, WPARAM wParam, HWND ventanaCerrar)
+{
+	if (LOWORD(wParam) == ID_EVENTOS)
+	{
+		ShowWindow(ventanaCerrar, 0);
+		ShowWindow(hAgendaWnd, 1);
+	}
+	if (LOWORD(wParam) == ID_MISCOMPRAS)
+	{
+		ShowWindow(ventanaCerrar, 0);
+		ShowWindow(hMisComprasWnd, 1);
+	}
+	if (LOWORD(wParam) == ID_SALIR)
+	{
+		DestroyWindow(hwnd);
+		PostQuitMessage(0);
+	}
+}
+
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevINstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+
+	hLogInWnd = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_LogIn), NULL, PROCLogIn);
+	hAgendaWnd = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_EVENTOS), NULL, PROCEventos);
+	hCompraWnd = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_COMPRA), NULL, PROCCompra);
+	hMisComprasWnd = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_MisCompras), NULL, PROCMisCompras);
+
+	ShowWindow(hLogInWnd, nCmdShow);
+
+	/* Bucle de mensajes: | ESPERA QUE HAY QUE HACER: sin este la ventana no "aparece" */
+	MSG msg = { 0 };
+	while (GetMessage(&msg, NULL, 0, 0)) 
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+}
---
+++ 3 PARCIAL/resource.h
@@ -0,0 +1,81 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by 3 PARCIAL.rc
+//
+#define IDD_LogIn                       101
+#define IDR_MENU1                       102
+#define IDD_EVENTOS                     104
+#define IDD_COMPRA                      106
+#define IDD_MisCompras                  108
+#define IDC_EDIT_LogIn_ClaveUsu         1001
+#define IDC_EDIT_LogIn_Contrasena       1002
+#define IDC_LISTBOX_Eventos             1003
+#define IDC_MONTHCALENDAR_Eventos       1004
+#define IDC_BUTTON_Eventos_Filtrar      1006
+#define IDC_STATIC_Eventos_Obra         1007
+#define IDC_STATIC_Eventos_Fecha        1008
+#define IDC_STATIC_Eventos_Hora         1009
+#define IDC_STATIC_Eventos_Clave        1010
+#define IDC_STATIC_Eventos_Locacion     1011
+#define IDC_CHECK_Eventos               1012
+#define IDCANCEL_Compra                 1013
+#define IDC_MONTHCALENDAR1              1014
+#define IDC_RADIO_A1                    1023
+#define IDC_RADIO_B2                    1024
+#define IDC_RADIO_C3                    1025
+#define IDC_RADIO_D4                    1026
+#define IDC_RADIO_E5                    1027
+#define IDC_RADIO_B1                    1028
+#define IDC_RADIO_C1                    1029
+#define IDC_RADIO_D1                    1030
+#define IDC_RADIO_E1                    1031
+#define IDC_RADIO_A2                    1032
+#define IDC_RADIO_C2                    1033
+#define IDC_RADIO_D2                    1034
+#define IDC_RADIO_E2                    1035
+#define IDC_RADIO_A3                    1036
+#define IDC_RADIO_B3                    1037
+#define IDC_RADIO_D3                    1038
+#define IDC_RADIO_E3                    1039
+#define IDC_RADIO_A4                    1040
+#define IDC_RADIO_B4                    1041
+#define IDC_RADIO_C4                    1042
+#define IDC_RADIO_E4                    1043
+#define IDC_RADIO_A5                    1044
+#define IDC_RADIO_B5                    1045
+#define IDC_RADIO_D5                    1046
+#define IDC_RADIO_C5                    1047
+#define IDC_STATIC_Compra_FECHA         1048
+#define IDC_STATIC_Compra_Hora          1049
+#define IDC_STATIC_Compra_NombreObra    1050
+#define IDC_STATIC_Compra_Descripcion   1051
+#define IDC_STATIC_Compras_Lugar        1052
+#define IDOK_Confirmar                  1053
+#define IDC_COMBO1                      1054
+#define IDC_LIST1                       1055
+#define IDC_LISTBOX_Boletos             1055
+#define IDC_BUTTON1                     1056
+#define IDC_BUTTON_MisCompras_Filtros   1056
+#define IDC_BUTTON_Eventos_Comprar      1059
+#define IDC_MONTHCALENDAR_MisCompras    1060
+#define IDC_STATIC_MisCompras_Nombre    1061
+#define IDC_STATIC_MisCompras_Fecha     1062
+#define IDC_STATIC_MisCompras_Hora      1063
+#define IDC_STATIC_MisCompras_Lugar     1064
+#define IDC_STATIC_MisCompras_Clave     1065
+#define IDC_STATIC_Compra_PrecioFinal   1066
+#define ID_EVENTOS                      40001
+#define ID_COMPRADEBOLETOS              40002
+#define ID_MISCOMPRAS                   40003
+#define ID_SALIR                        40004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        110
+#define _APS_NEXT_COMMAND_VALUE         40005
+#define _APS_NEXT_CONTROL_VALUE         1067
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
